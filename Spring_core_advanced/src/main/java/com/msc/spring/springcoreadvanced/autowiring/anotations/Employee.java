220756be6ed48c60a013ea4a0ceefa976ca323a1
Marllon Costa <marllonsc@gmail.com>
2020-09-08 13:42:32 -0300

    created modules organization

---
+++ Spring_core_advanced/src/main/java/com/msc/spring/springcoreadvanced/autowiring/anotations/Employee.java
@@ -0,0 +1,31 @@
+package com.msc.spring.springcoreadvanced.autowiring.anotations;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+
+public class Employee {
+
+    @Autowired()
+    @Qualifier("address3")
+    private Address address;
+
+    public Employee(Address address) {
+        this.address = address;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "address=" + address +
+                '}';
+    }
+}
