bc9ac2cf3b9381d45d23b9af192e975512557ff6
Marllon Costa <marllonsc@gmail.com>
2020-09-10 15:47:55 -0300

    finished 19 Using ModelMap and String View

--- Spring_mvc/src/main/java/com/msc/spring/controller/UserController.java
+++ Spring_mvc/src/main/java/com/msc/spring/controller/UserController.java
@@ -2,6 +2,7 @@ package com.msc.spring.controller;
 
 import com.msc.spring.dto.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,17 +12,18 @@ import org.springframework.web.servlet.ModelAndView;
 public class UserController {
 
     @RequestMapping("/registrationPage")
-    public ModelAndView showRegistrationPage(){
-        ModelAndView modelAndView = new ModelAndView("userReg");
-        return modelAndView;
+    public String showRegistrationPage(){
+       // ModelAndView modelAndView = new ModelAndView("userReg");
+        return "userReg";
     }
 
     @RequestMapping(value = "registerUser", method = RequestMethod.POST)
-    public ModelAndView registerUser(@ModelAttribute("user") User user){
+    public String registerUser(@ModelAttribute("user") User user, ModelMap model){
         System.out.println(user);
-        ModelAndView modelAndView = new ModelAndView("regResult");
-        modelAndView.addObject("user",user);
-        return modelAndView;
+        //ModelAndView modelAndView = new ModelAndView("regResult");
+       // modelAndView.addObject("user",user);
+        model.addAttribute("user",user);
+        return "regResult";
     }
 
 
